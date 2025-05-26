dcc5b438d9b8a10e8566126728dab438292f1147
Pamela <karimipamela64@gmail.com>
2025-05-26 11:11:41 +0300

    Login and Register Page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_button.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
+import 'package:food_delivery_app/pages/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  const LoginPage({super.key, this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //text editing controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  //login method
+  void login() {
+    //authetication
+
+    //navigate to homepage
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const HomePage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Icon(
+            Icons.lock_open_rounded,
+            size: 72,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          const SizedBox(height: 25),
+          //message, app slogan
+          Text(
+            "Food delivery App",
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          const SizedBox(height: 25),
+          //email textfield
+          MyTextfield(
+            controller: emailController,
+            hintText: "Email",
+            obscureText: false,
+          ),
+          const SizedBox(height: 25),
+          MyTextfield(
+            controller: passwordController,
+            hintText: "Password",
+            obscureText: true,
+          ),
+          //sign in button
+          const SizedBox(height: 25),
+          MyButton(text: "Sign In", onTap: login),
+          const SizedBox(height: 25),
+          //not  a member? register now
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Not a member?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              const SizedBox(width: 4),
+              GestureDetector(
+                onTap: widget.onTap,
+                child: Text(
+                  "Register now",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_button.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
+
+class RegisterPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  const RegisterPage({super.key, this.onTap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  //text editing controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Icon(
+            Icons.lock_open_rounded,
+            size: 72,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          const SizedBox(height: 25),
+          //message, app slogan
+          Text(
+            "Let's create an account",
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          const SizedBox(height: 25),
+          //email textfield
+          MyTextfield(
+            controller: emailController,
+            hintText: "Email",
+            obscureText: false,
+          ),
+          const SizedBox(height: 10),
+          MyTextfield(
+            controller: passwordController,
+            hintText: "Password",
+            obscureText: true,
+          ),
+            const SizedBox(height: 10),
+          MyTextfield(
+            controller: confirmPasswordController,
+            hintText: "Confirm Password",
+            obscureText: true,
+          ),
+          //Log in button
+          const SizedBox(height: 25),
+          MyButton(text: "Sign Up", onTap: () {}),
+          const SizedBox(height: 25),
+          //already have an account
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already have an account?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              const SizedBox(width: 4),
+              GestureDetector(
+                onTap: widget.onTap,
+                child: Text(
+                  "Login now",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
